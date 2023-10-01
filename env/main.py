211bba3ee0986c890d28c19f3f8df8ea2cd8ef8e
Bram Lahousse <70818886+LahousseBram@users.noreply.github.com>
2023-10-01 23:52:29 +0200

    Typo

--- env/main.py
+++ env/main.py
@@ -46,8 +46,7 @@ def open_bch(driver):
 
 def open_dice(driver):
     try:
-        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div[1]/a[5]/div[1]'))).click()
-        time.sleep(2)
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div[1]/a[5]/div[1]'))).click()
         bet_dice(driver=driver)
     except:
         reset_program(driver=driver)
