820092ce55c4b284ef5c2ef383af04fad70fbfd2
Bram Lahousse <70818886+LahousseBram@users.noreply.github.com>
2023-10-02 13:58:21 +0200

    Added post request to API for logging

--- env/main.py
+++ env/main.py
@@ -84,6 +84,14 @@ def bet_dice(driver):
 
     current_money = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/header/div/a/div").get_attribute("title")
 
+    URL = "94.110.172.3:5000/add-statistic"
+    PARAMS = {
+        "current_streak": current_streak,
+        "high_score": high_score,
+        "current_bet": amount_of_bets
+    }
+    requests.post(URL, params=PARAMS)
+
     print(current_money)
 
     if "loading" in current_money:
