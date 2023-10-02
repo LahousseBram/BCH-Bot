cc7fe98ecd137c5c130a5a9c9b863e6a810ff8da
Bram Lahousse <70818886+LahousseBram@users.noreply.github.com>
2023-10-02 09:27:43 +0200

    typo

--- env/main.py
+++ env/main.py
@@ -21,7 +21,7 @@ def refresh_bch(driver, wait=False):
     if wait:
         time.sleep(5)
     driver.execute_script("localStorage.clear()")
-    driver.get("https://bch.games")
+    driver.get("https://bch.games/")
     open_dice(driver=driver)
 
 def hard_reset(driver):
@@ -46,6 +46,7 @@ def open_bch(driver):
 
 def open_dice(driver):
     try:
+        print("opening dice")
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div[1]/a[5]/div[1]'))).click()
         bet_dice(driver=driver)
     except:
@@ -64,18 +65,20 @@ def bet_dice(driver):
     print("==================")
 
     try:
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div/div[1]/div/div[4]/div/div/div[2]/div/div[1]/input')))
         if driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div/div[1]/div/div[4]/div/div/div[2]/div/div[1]/input') == '15.00000000':
             time.sleep(5)
             current_streak = 0
             refresh_bch(driver=driver)
     except:
         current_streak = 0
+        print("test")
         refresh_bch(driver=driver)
 
     WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div/div[1]/div/div[4]/div/div/div[3]/button[2]'))).click()
     WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div/div[1]/div/div[4]/div/div/div[3]/button'))).click()
     WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/main/div/div/div[1]/div/div[3]/button/div[2]'))).click()
-    time.sleep(2)
+    time.sleep(0.5)
 
     current_money = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/header/div/a/div").get_attribute("title")
 
@@ -105,6 +108,8 @@ def bet_dice(driver):
 def main():
     options = uc.ChromeOptions()
     options.add_argument('--disable-popup-blocking')
+    options.add_argument('--incognito')
+    options.add_argument('--start-maximized')
 
     driver = uc.Chrome(options=options)
     open_bch(driver)
